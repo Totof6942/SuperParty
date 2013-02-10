da4862f55b4aa5578adead352f407eab9fc3345a
Totof6942 <poulette.christophe92@gmail.com>
2013-02-10 16:49:03 +0100

    Add tempaltes for 404 and 500 erros.

--- src/app.php
+++ src/app.php
@@ -17,39 +17,5 @@ $app->register(new TwigServiceProvider(), array(
     // 'twig.options' => array('cache' => __DIR__.'/../cache/twig'),
 ));
 
-/**
- * Home page
- */
-$app->get('/', function() use($app) {
-	return $app['twig']->render('index.html', array(
-			'parties' => $app['db']->fetchAll('SELECT id, name, date FROM parties'),
-		));
-})
-->bind('homepage');
-
-/**
- * All locations
- */
-$app->get('/locations', function() use($app) {
-    return $app['twig']->render('locations.html', array());
-})
-->bind('locations');
-
-/**
- * One location
- */
-$app->get('/locations/{id}', function($id) use($app) {
-    return $app['twig']->render('location.html', array());
-})
-->bind('location');
-
-/**
- * All parties
- */
-$app->get('/parties', function() use($app) {
-    return $app['twig']->render('parties.html', array());
-})
-->bind('parties');
-
 
 return $app;
