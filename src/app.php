3a93e5eed271cabf36fe49aefb004876cb2a118b
Totof6942 <poulette.christophe92@gmail.com>
2013-02-10 21:47:11 +0100

    Start login.

--- src/app.php
+++ src/app.php
@@ -1,21 +1,37 @@
 <?php
 
 use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
-use Silex\Provider\ServiceControllerServiceProvider;
-use Silex\Provider\DoctrineServiceProvider;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
 
 $app = new Application();
 $app->register(new UrlGeneratorServiceProvider());
+$app->register(new SessionServiceProvider());
+$app->register(new FormServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new DoctrineServiceProvider());
+
 $app->register(new TwigServiceProvider(), array(
     'twig.path'    => array(__DIR__.'/../templates'),
     // 'twig.options' => array('cache' => __DIR__.'/../cache/twig'),
 ));
+
+$app->register(new TranslationServiceProvider());
+$app['translator'] = $app->share($app->extend('translator', function($translator, $app) {
+    $translator->addLoader('yaml', new YamlFileLoader());
+
+    $translator->addResource('yaml', __DIR__.'/../resources/locales/fr.yml', 'fr');
+
+    return $translator;
+}));
 
 
 return $app;
--- src/controllers.php
+++ src/controllers.php
@@ -5,6 +5,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Form\FormError;
 
 /**
  * Home page
@@ -15,6 +17,66 @@ $app->get('/', function() use($app) {
 		));
 })
 ->bind('homepage')
+;
+
+/**
+ * Login
+ */
+$app->match('/login', function() use ($app) {
+
+    $form = $app['form.factory']->createBuilder('form')
+        ->add('email', 'email', array(
+            'label'       => 'Email',
+            'constraints' => array(
+                new Constraints\NotBlank(),
+                new Constraints\Email(),
+            ),
+        ))
+        ->add('password', 'password', array(
+            'label'       => 'Password',
+            'constraints' => array(
+                new Constraints\NotBlank(),
+            ),
+        ))
+        ->getForm()
+    ;
+
+    if ('POST' === $app['request']->getMethod()) {
+        $form->bindRequest($app['request']);
+
+        if ($form->isValid()) {
+
+            $email    = $form->get('email')->getData();
+            $password = $form->get('password')->getData();
+
+            if ('email@example.com' == $email && 'password' == $password) {
+                $app['session']->set('user', array(
+                    'email' => $email,
+                ));
+
+                $app['session']->setFlash('notice', 'You are now connected');
+
+                return $app->redirect($app['url_generator']->generate('homepage'));
+            }
+
+            $form->addError(new FormError('Email / password does not match (email@example.com / password)'));
+        }
+    }
+
+    return $app['twig']->render('login.html', array('form' => $form->createView()));
+})
+->bind('login')
+;
+
+/**
+ * Logout
+ */
+$app->match('/logout', function() use ($app) {
+    $app['session']->clear();
+
+    return $app->redirect($app['url_generator']->generate('homepage'));
+})
+->bind('logout')
 ;
 
 /**
@@ -32,6 +94,7 @@ $app->get('/locations', function() use($app) {
 $app->get('/locations/{id}', function($id) use($app) {
     return $app['twig']->render('location.html', array());
 })
+->assert('id', '\d+')
 ->bind('location')
 ;
 
