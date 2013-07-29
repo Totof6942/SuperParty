81347a4630e8b76580affde6e0ef9fd29aa3f209
Christophe Poulette <poulette.christophe92@gmail.com>
2013-07-29 23:38:10 +0200

    Remove debug function.

--- web/index.php
+++ web/index.php
@@ -1,12 +1,5 @@
 <?php
 
-function debug($var)
-{
-    echo '<pre>';
-    print_r($var);
-    echo '</pre>';
-}
-
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__.'/../src/app.php';
